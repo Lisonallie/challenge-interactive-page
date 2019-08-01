1539fdf62b249c91b562a47df13e4ab26b449654
Lisonallie <allisonvanlinden@gmail.com>
2019-08-01 13:11:04 +0200

    add show functions that work

--- app.js
+++ app.js
@@ -6,24 +6,36 @@ let summary = document.getElementById("summary");
 let practical = document.getElementById("practical");
 let contact = document.getElementById("contact");
 
-function showTab1() {
-    if (summary.style.display === "none") {
-        summary.style.display === "block";
-    }
-}
-
-function showTab2() {
-    if (practical.style.display === "none") {
-        practical.style.display === "block";
-    }
-}
-
-function showTab3() {
-    if (contact.style.display === "none") {
-        contact.style.display === "block";
-    }
+window.onload = function() {
+    summary.style.display = "block";
+    practical.style.display = "none";
+    contact.style.display = "none";
 }
 
 tab1.addEventListener("click", showTab1);
 tab2.addEventListener("click", showTab2);
 tab3.addEventListener("click", showTab3);
+
+function showTab1() {
+    if (summary.style.display == "block") {
+        summary.style.display = "none";
+    } else if (summary.style.display == "none") {
+        summary.style.display = "block";
+    }
+}
+
+function showTab2() {
+    if (practical.style.display == "none") {
+        practical.style.display = "block";
+    } else if (practical.style.display == "block") {
+        practical.style.display = "none";
+    }
+}
+
+function showTab3() {
+    if (contact.style.display == "none") {
+        contact.style.display = "block";
+    } else if (contact.style.display == "block") {
+        contact.style.display = "none";
+    }
+}
