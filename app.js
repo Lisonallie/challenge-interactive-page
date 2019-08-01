483fc36fd247cdfa03a22269522b2eff6227f7e4
Lisonallie <allisonvanlinden@gmail.com>
2019-08-01 10:57:17 +0200

    add first variable definitions

---
+++ app.js
@@ -0,0 +1,7 @@
+let tab1 = document.getElementById("tab1");
+let tab2 = document.getElementById("tab2");
+let tab3 = document.getElementById("tab3");
+
+let summary = document.getElementById("summary");
+let practical = document.getElementById("practical");
+let contact = document.getElementById("contact");
